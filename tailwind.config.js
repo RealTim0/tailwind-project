5297f6d5702eaf42918915c2034f61cb0754ead9
RealTim0 <gemtimo994@gmail.com>
2023-05-26 11:49:05 +0400

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html", ".//js/*.js"],
+  theme: {
+    extend: {
+      screens: {
+        'widescreen': {'raw':'(min-aspect-ratio:3/2)'},
+        'tallscreen': {'raw':'(max-aspect-ratio:13/20)'}, 
+        },
+        keyframes: {
+          'open-menu':{
+            '0%';{
+              Transform; 'scaleY(0)' }
+            '80%';{
+              Transform; 'scaleY(1.2)'}
+            '100%';{
+              Transform; 'scaleY(1)'
+            }
+            animation: {
+              'open-menu': 'open-menu 0.5s ease-in-out forwards',
+             }
+          },
+        },
+  plugins: [],
+    }
+
